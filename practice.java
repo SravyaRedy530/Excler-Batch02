5161d12739f77f1b10b617e6f4760c003816403f
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-03 10:16:13 +0530

    Add files via upload
    practice code

---
+++ practice.java
@@ -0,0 +1,17 @@
+public class practice {
+    public static void main(String[] args){
+        
+        String username = "sravyareddy";
+        String password = "sravya530@gmail.com";
+    if((username=="sravyareddy") && (password=="sravya530@gmail.com")){
+        System.out.println("Pass the username and password:");
+    }else{
+        System.out.println("Do NOT Pass the username and password:");
+    }
+        while((username=="sravyareddy") && (password=="sravya530@gmail.com")){
+            System.out.println("hello Happy new year");
+            break;
+    }
+}
+}
+    
