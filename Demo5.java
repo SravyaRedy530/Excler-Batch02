973934980cca7b219058a183ebb38f4afdb2f473
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-03 09:54:24 +0530

    Add files via upload
    While loop

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String[] args){
+    String username = "sravyareddy";
+    String password = "sravya530@gmail.com";
+    while((username=="sravyareddy") && (password=="sravya530@gmail.com")){
+    System.out.println("hello Happy new year");
+    break;
+    }
+} 
+}
