500b25e9d33aec6fe8c9c75ce1a7f04483da67d0
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-07 10:09:37 +0530

    Add files via upload

---
+++ Decrement.java
@@ -0,0 +1,13 @@
+public class Decrement {
+    public class incrementdecrement {
+        public static void main(String[] args) {
+            // Drecrement operators
+            int x=1;
+            
+            x--;
+            System.out.println(x);
+        }
+        
+    }
+    
+}
