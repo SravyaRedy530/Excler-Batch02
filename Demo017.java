22c49fe59fd0937c886a29f97b476c70149b2ccf
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-26 16:32:04 +0530

    Sum of digits
    Sum of digits

--- Demo017.java
+++ Demo017.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class Demo017 {
     public static void main(String[] args) {
-        // Create a scanner object to take input from the user
         Scanner scanner = new Scanner(System.in);
         
         // Prompt the user to enter a number
         System.out.print("Enter a two-digit number: ");
         int number = scanner.nextInt();
-        
-        // Get the tens and ones place digits
+    
         int tens = number / 10;  // Extract the tens digit
         int ones = number % 10;  // Extract the ones digit
         
